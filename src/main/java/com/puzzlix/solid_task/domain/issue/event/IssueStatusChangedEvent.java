a5b082f43675f2ec6e4543a66569ae8b399313c2
whwhwjddn123 <whwhwjddn123@gmail.com>
2025-10-15 14:20:21 +0900

    sms api 전송 인증

--- src/main/java/com/puzzlix/solid_task/domain/issue/event/IssueStatusChangedEvent.java
+++ src/main/java/com/puzzlix/solid_task/domain/issue/event/IssueStatusChangedEvent.java
@@ -4,10 +4,6 @@ import com.puzzlix.solid_task.domain.issue.Issue;
 import lombok.Data;
 
 // 스프링에서 제공하는 스프링 이벤트를 사용할 예정
-/*
-* */
-
-
 
 @Data
 public class IssueStatusChangedEvent {
--- src/main/java/com/puzzlix/solid_task/domain/notification/SmsNotificationSender.java
+++ src/main/java/com/puzzlix/solid_task/domain/notification/SmsNotificationSender.java
@@ -1,13 +1,39 @@
 package com.puzzlix.solid_task.domain.notification;
 
+import com.solapi.sdk.SolapiClient;
+import com.solapi.sdk.message.exception.SolapiEmptyResponseException;
+import com.solapi.sdk.message.exception.SolapiMessageNotReceivedException;
+import com.solapi.sdk.message.exception.SolapiUnknownException;
+import com.solapi.sdk.message.model.Message;
+import com.solapi.sdk.message.service.DefaultMessageService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SmsNotificationSender implements NotificationSender{
+
+    DefaultMessageService messageService =
+            SolapiClient.INSTANCE
+                    .createInstance("NCS7OISBDF6ULLIP", "SFMLERJKS4RX1GVLW3I7FWSRO3BP7XUM");
+
+
     @Override
     public void send(String message) {
         // SMS 외부 API 연동 기능 처리
+        Message messageTo = new Message();
+        messageTo.setFrom("010-5207-6426");
+        messageTo.setTo("010-5277-0535");
+        messageTo.setText("이승민입니다");
         System.out.println("SMS 알림 발송: " + message);
+        try {
+            messageService.send(messageTo);
+        } catch (SolapiMessageNotReceivedException e) {
+            throw new RuntimeException(e);
+        } catch (SolapiEmptyResponseException e) {
+            throw new RuntimeException(e);
+        } catch (SolapiUnknownException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
